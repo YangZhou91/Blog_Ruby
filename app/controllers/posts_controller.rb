300c2df7e3ae4031075d64ebf9f20bede70606fb
Gavin <gavinzhou10c@gmail.com>
2013-12-22 07:29:51 -0500

    Fix the no route for new problem. The new function in the post controller has been updated.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
    
   def new
+    @post = Post.new
   end 
    
   def create
     @post = Post.new(post_params)
+    
     @post.save
     redirect_to @post
   end
